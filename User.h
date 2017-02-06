daaa15c322966f85eadb2c286f6e47a62d5fc163
bastieb <bastie.b@web.de>
2017-02-06 12:46:47 +0100

    tobias Version nach dem Telefonat mit Bastie über .read .write

--- User.h
+++ User.h
@@ -18,12 +18,12 @@ SC_MODULE(User){
 	sc_out<double> v_car;
 	sc_out<bool>  car_sighted;
 	//Variablen
-	int i,random, random2, randomzeit, random_old;
+	int i,random, randomzeit;
 
 	//Prozesse
 	SC_CTOR(User){
 		B_start.initialize(0);
-		//p_gas.initialize(0);
+		p_gas.initialize(0);
 		p_bremse.initialize(0);
 		B_set.initialize(0);
 		car_sighted.initialize(false); 
@@ -40,7 +40,7 @@ SC_MODULE(User){
 	while(i<20){				
 		//zufallszahlen generieren
 		randomzeit = rand() %16;
-		random = rand() %9;
+		random = rand() %11;
 
 		//Auto anschalten
 		if(random==0 && B_stop ==1){		
@@ -48,69 +48,71 @@ SC_MODULE(User){
 		B_stop=0, B_start=1;				
 		wait(2,SC_SEC);
 				}
-		//Pedalaenderung		
+		//Pedalaenderung
+		//Gas geben
 		if(random==1 && randomzeit > 9 && B_stop ==0){
-			cout << "Pedalanederung" << endl;	
-			wait(randomzeit,SC_SEC);
+			wait(randomzeit, SC_SEC);
+			cout << "Gas geben" << endl;
+			p_bremse=0;p_gas=1+rand() %100;
+			}
+		//Bremsen
+		if(random==2 && randomzeit > 9 && B_stop ==0){
+			wait(randomzeit, SC_SEC);
+			cout << "Bremsen" << endl;
+			p_gas=0;p_bremse=1+rand() %100;
+			}
+		//Rollen
+		if(random==3 && randomzeit > 9 && B_stop ==0){
+			wait(randomzeit, SC_SEC);
+			cout << "Rollen" << endl;
 			p_gas=0,p_bremse=0;
-			//neuer Zufall für Pedalaktionen
-			//while (random !=2){
-				random2= rand()%2;
-cout << "Gas bei 1 bremse bei 0:" << random2 << endl;
-				//Gas geben			
-				if(random2==1 && p_gas<100)p_gas = p_gas + 10;   
-				//Bremsen			
-				if(random2==0 && p_bremse<100){p_bremse = p_bremse + 10;B_set=0; }
-
-				cout << "Bremse ist " << p_bremse << ", Gas ist " << p_gas << endl;
-			//}
-		}
+			}
 		
 		//Tempomat anschalten
-		if(random==2 && tempomatstatus ==0 && p_bremse==0 && B_stop ==0 && randomzeit > 1){
-				wait(2, SC_SEC);
+		if(random==4 && tempomatstatus ==0 && p_bremse==0 && randomzeit > 1){
+				wait(randomzeit, SC_SEC);
 				cout << "Tempomat wird angeschalten" << endl;
 				B_set=1;wait(1, SC_SEC);
 				B_set=0; //Er lässt den Knop wieder los
 				}
 		//v_d erhöhen
-		if(random==3 && tempomatstatus == 1 && randomzeit > 4){
-				wait(5, SC_SEC);				
+		if(random==5 && tempomatstatus == 1 && randomzeit > 4){
+				wait(randomzeit, SC_SEC);				
 				cout << "Wunschgeschwindigkeit wird um 1 erhöht" << endl;
 				B_vp=1;
 				//B_vp wieder Null setzen
 				wait(1, SC_SEC); B_vp=0;
 				}
 		//v_d verringern
-		if(random==4 && tempomatstatus == 1 && randomzeit > 4){
-				wait(5, SC_SEC);				
+		if(random==6 && tempomatstatus == 1 && randomzeit > 4){
+				wait(randomzeit, SC_SEC);				
 				cout << "Wunschgeschwindigkeit wird um 1 verringert" << endl;
 				B_vm=1;
 				//B_vm wieder Null setzen
 				wait(1, SC_SEC); B_vm=0;			
 				}
 		//Auto ausschalten
-		if(random==5 && B_stop ==0){				//Auto ausschalten
+		if(random==7 && B_stop ==0){				//Auto ausschalten
 				cout << "Nutzer schaltet das Auto aus" << endl;				
 				p_gas=0,p_bremse=0;
 				B_stop=1, B_start=0;
 				wait(2,SC_SEC);			//Auto bleibt 2 Sekunden aus			
 					}
 		//Sensor sichtet ein Auto
-		if(random==6 && tempomatstatus == 1 && dist>300){
+		if(random==8 && tempomatstatus == 1 && dist>300){
 				car_sighted=true; 			
 				v_car=rand() %50;
 				cout << "Es wurde ein Auto mit der Geschwindigkeit " << v_car << " gesichtet" << endl;		
 				wait(1,SC_SEC);		
 				}
 		//FrontCar beschleunigt um 1 m/s
-		if(random==7 && dist<300 && v_car<50){ 			
+		if(random==9 && dist<300 && v_car<50){ 			
 				v_car=v_car+1;
 				cout << "FrontCar beschleunigt v_car um 1 m/s "  << endl;				
 				wait(1,SC_SEC);				
 				}
 		//FrontCar bremst um 1 m/s
-		if(random==8 && dist<300 && v_car>5){ 			
+		if(random==10 && dist<300 && v_car>5){ 			
 				v_car=v_car-1;
 				cout << "FrontCar verringtert v_car um 1 m/s "  << endl;				
 				wait(1,SC_SEC);				
@@ -118,7 +120,6 @@ cout << "Gas bei 1 bremse bei 0:" << random2 << endl;
 		//Wenn es keine Möglichkeit gibt	
 		else i--; 			
 		i++;
-		random_old = random;
 			}
 
 	}
